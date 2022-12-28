bdd09b8d29689a91ef88230b81f2ceada4b92701
Dwiki Ramadhan <dwikibyy@gmail.com>
2022-12-28 14:05:02 +0700

    add auto login & auto logout

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -1,30 +1,26 @@
+let timer;
+
 export default {
   async login(context, payload) {
-    const response = await fetch(process.env.VUE_APP_LOGIN, {
-      method: "POST",
-      body: JSON.stringify({
-        email: payload.email,
-        password: payload.password,
-        returnSecureToken: true,
-      }),
-    });
-    const responseData = await response.json();
-    if (!response.ok) {
-      console.log(responseData);
-      const error = new Error(
-        responseData.message || "Failed to authenticated"
-      );
-      throw error;
-    }
-    console.log(responseData);
-    context.commit("setUser", {
-      token: responseData.idToken,
-      userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
+    return context.dispatch("auth", {
+      ...payload,
+      mode: "login",
     });
   },
   async signup(context, payload) {
-    const response = await fetch(process.env.VUE_APP_SIGNUP, {
+    return context.dispatch("auth", {
+      ...payload,
+      mode: "signup",
+    });
+  },
+  async auth(context, payload) {
+    const mode = payload.mode;
+    let url = process.env.VUE_APP_LOGIN;
+
+    if (mode === "signup") {
+      url = process.env.VUE_APP_SIGNUP;
+    }
+    const response = await fetch(url, {
       method: "POST",
       body: JSON.stringify({
         email: payload.email,
@@ -34,24 +30,66 @@ export default {
     });
     const responseData = await response.json();
     if (!response.ok) {
-      console.log(responseData);
       const error = new Error(
         responseData.message || "Failed to authenticated"
       );
       throw error;
     }
-    console.log(responseData);
+
+    const expiresIn = +responseData.expiresIn * 1000;
+    const expirationDate = new Date().getTime() + expiresIn;
+
+    //save token, id user, expiration in local storage
+    localStorage.setItem("token", responseData.idToken);
+    localStorage.setItem("userId", responseData.localId);
+    localStorage.setItem("tokenExpiration", expirationDate);
+
+    // auto logout
+    timer = setTimeout(() => {
+      context.dispatch("autoLogout");
+    }, expiresIn);
+
     context.commit("setUser", {
       token: responseData.idToken,
       userId: responseData.localId,
-      tokenExpiration: responseData.expiresIn,
     });
   },
+  tryLogin(context) {
+    const token = localStorage.getItem("token");
+    const userId = localStorage.getItem("userId");
+    const tokenExpiration = localStorage.getItem("tokenExpiration");
+
+    const expiresIn = +tokenExpiration - new Date().getTime();
+
+    if (expiresIn < 0) {
+      return;
+    }
+
+    timer = setTimeout(() => {
+      context.dispatch("autoLogout");
+    }, expiresIn);
+
+    if (token && userId) {
+      context.commit("setUser", {
+        token: token,
+        userId: userId,
+      });
+    }
+  },
   logout(context) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("userId");
+    localStorage.removeItem("tokenExpiration");
+
+    clearTimeout(timer);
+
     context.commit("setUser", {
       token: null,
       userId: null,
-      tokenExpiration: null,
     });
+  },
+  autoLogout(context) {
+    context.dispatch("logout");
+    context.commit("setAutoLogout");
   },
 };
