62159e3098c3e5ccb074ab60e9f96be465c88874
Dwiki Ramadhan <dwikibyy@gmail.com>
2022-12-28 11:39:32 +0700

    attaching token to outgoing request

--- src/store/modules/requests/actions.js
+++ src/store/modules/requests/actions.js
@@ -27,8 +27,10 @@ export default {
   },
   async fetchRequests(context) {
     const coachId = context.rootGetters.userId;
+    const token = context.rootGetters.token;
     const response = await fetch(
-      `https://vue-http-demo-42cc8-default-rtdb.firebaseio.com/request/${coachId}.json`
+      `https://vue-http-demo-42cc8-default-rtdb.firebaseio.com/request/${coachId}.json?auth=` +
+        token
     );
     const responseData = await response.json();
 
